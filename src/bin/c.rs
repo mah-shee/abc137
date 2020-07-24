f5dd0007e4e33374fd5934515024f8dd89f64ec7
鎌田正聖 <masatoshi@masa-macbook-pro.local>
2020-07-24 20:56:57 +0900

    Done c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -1,8 +1,28 @@
 #[allow(unused_imports)]
 use proconio::marker::{Bytes, Chars, Usize1};
 use proconio::{fastout, input};
+use std::collections::HashMap;
 #[fastout]
 fn main() {
     input! {
+        n: usize,
     }
+    let mut map = HashMap::new();
+    let mut ans = 0;
+    for _ in 0..n {
+        input! {
+            mut s: Chars,
+        }
+        s.sort();
+        let sorted_string = s.into_iter().rev().collect::<String>();
+        map.entry(sorted_string)
+            .and_modify(|e| *e += 1)
+            .or_insert(1);
+    }
+
+    for (_, v) in &map {
+        ans += v * (v - 1) / 2;
+    }
+
+    println!("{}", ans);
 }
